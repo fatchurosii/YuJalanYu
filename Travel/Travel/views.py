a79f05a756065d2f3584b7030e804d9d67fa99ba
unknown <yosuapurnomo321@gmail.com>
2020-11-06 23:49:58 +0700

    getImages

--- Travel/Travel/views.py
+++ Travel/Travel/views.py
@@ -9,14 +9,21 @@ class homeView(View):
 
 	def get(self, request):
 		model = modelPaket.objects.order_by('-tgl_dibuat')
-		# model.images
+		modelImg = imagesPaket.objects.all()
+		data = modelImg.values('id_paket').distinct()
+		
 		dataPaket = []
 		dataImg = []
 		for x in model:
 			if x.tgl_berangkat > datetime.date.today():
 				dataPaket.append(x)
-				# dataImg.append(imagesPaket.objects.get(id_paket=x))
 		
+		for x in modelImg.values('id_paket').distinct():
+			print(x['id_paket'])
+			oper = modelImg.filter(id_paket=x['id_paket'])
+			dataImg.append(oper[0])
+				
+		print(dataImg)
 		context = {
 		'title':"HOME",
 		'data':dataPaket,
