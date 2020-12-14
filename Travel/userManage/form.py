58f254168a4f9d2a95609a36a04b03e33399a9f0
unknown <yosuapurnomo321@gmail.com>
2020-12-14 22:07:35 +0700

    createFormUser

--- Travel/userManage/form.py
+++ Travel/userManage/form.py
@@ -1,10 +1,35 @@
 from django import forms
-from .models import modelUser
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
-class formUser(forms.ModelForm):
-
+class formUser(UserCreationForm):
 	class Meta:
-		model = modelUser
-		fields = '__all__'
+		model = User
+		fields = ["username", "email"]
+
+	def clean_password2(self):
+		print("Ini Password 1")
+		password1 = self.cleaned_data.get("password1")
+		password2 = self.cleaned_data.get("password2")
+		print(password1)
+		if password1 is password2:
+			print(True)
+			return password2
+		else:
+			print(False)
+			raise forms.ValidationError("Password Salah : ")
+
+	# def clean_password2(self):
+	# 	print("Ini Password 2")
+	# 	password1 = self.cleaned_data.get("password1")
+	# 	password2 = self.cleaned_data.get("password2")
+		
+	# 	if password1 is password2:
+	# 		print(True)
+	# 		return password1
+	# 	else:
+	# 		print(False)
+	# 		raise forms.ValidationError("Password Salah : ")
+
 	
