f4964e116027d42c41c197b64f9554fff158988c
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-01-14 20:53:20 +0700

    tes

--- Travel/Travel/settings.py
+++ Travel/Travel/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'r27!npj4#macmx73gxhe6-+dr!212yjp12h!&pwnw&478@)*1w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['yujalanyu-tes.herokuapp.com', '127.0.0.1',]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yujalanyu.site', '127.0.0.1',]
+# ALLOWED_HOSTS = []
 
 # Application definition
 
